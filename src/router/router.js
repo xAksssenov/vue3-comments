5afbfdcbd0c5731446ddad7d5c49749c4b775a5d
xAksssenov <aks.kirill32@gmail.com>
2023-12-06 22:48:52 +0300

    router + create page CommentPage.vue

---
+++ src/router/router.js
@@ -0,0 +1,18 @@
+import CommentPage from '@/page/CommentPage.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/Comment',
+    component: CommentPage,
+  }
+]
+
+console.log("Routes:", routes);
+
+const router = createRouter({
+    routes,
+    history: createWebHistory()
+})
+
+export default router
